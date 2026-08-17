2ff3bea4064e5b01e3c4173aebb048bd5a98e344
Darya <zorinadara642@gmail.com>
2026-08-17 15:50:35 +0400

    added Задание 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,12 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
 
         System.out.println();
-        System.out.println(service.calculate(1000000, 9.99, 1));
+        System.out.println(service.calculate(1000000, 9.99, 1) + " рублей");
 
         System.out.println();
-        System.out.println(service.calculate(1000000, 9.99, 2));
+        System.out.println(service.calculate(1000000, 9.99, 2) + " рублей");
 
         System.out.println();
-        System.out.println(service.calculate(1000000, 9.99, 3));
+        System.out.println(service.calculate(1000000, 9.99, 3) + " рублей");
     }
 }
