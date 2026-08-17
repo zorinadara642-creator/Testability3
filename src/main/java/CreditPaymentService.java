8dcb464b104b18c7cee49b561f8c97b3d6c7c96b
Darya <zorinadara642@gmail.com>
2026-08-17 15:08:22 +0400

    Задание3

---
+++ src/main/java/CreditPaymentService.java
@@ -0,0 +1,9 @@
+public class CreditPaymentService {
+    public int calculate(int amount, double annualRate, int yaers) {
+        double monthlyRate = annualRate / 12 / 100;
+        int months = yaers * 12;
+
+        double payment = amount * (monthlyRate * Math.pow(1 + monthlyRate, months)) / (Math.pow(1 + monthlyRate, months) -1);
+        return (int) payment;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+
+        System.out.println();
+        System.out.println(service.calculate(1000000, 9.99, 1));
+
+        System.out.println();
+        System.out.println(service.calculate(1000000, 9.99, 2));
+
+        System.out.println();
+        System.out.println(service.calculate(1000000, 9.99, 3));
+    }
 }
